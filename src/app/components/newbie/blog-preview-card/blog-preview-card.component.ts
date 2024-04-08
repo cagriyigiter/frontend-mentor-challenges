ae9a5be376b4a4360f0a6648311d1f917fd2e73e
cagriyigiter <77109819+cagriyigiter@users.noreply.github.com>
2024-04-08 17:27:40 +0000

    Build: (2bb0200) Merge pull request #7 from cagriyigiter/cagriyigiter-patch-1
    publisher job update

--- src/app/components/newbie/blog-preview-card/blog-preview-card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-blog-preview-card',
-  templateUrl: './blog-preview-card.component.html',
-  styleUrl: './blog-preview-card.component.scss',
-})
-export class BlogPreviewCardComponent {}
